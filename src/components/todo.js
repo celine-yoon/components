53bb4727e613a63cbfee2b76eb4b857af2bf13e7
Celine Yoon <celine.sy.yoon@gmail.com>
2023-04-01 00:41:44 +0900

    First Commit

---
+++ src/components/todo.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function TodoList() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (todo === "") {
+      return;
+    }
+    setTodos((current) => [todo, ...current]);
+    setTodo("");
+  };
+
+  return (
+    <div>
+      <h1>To Do List ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder=" Write your to do..."
+        />
+        <button type="submit">Add To Do</button>
+        <ul>
+          {todos.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+export default TodoList;
